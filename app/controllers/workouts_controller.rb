da5b09b2f3b5cc6a12b8963a72e303d124c97eb9
Dane-JW <thedanehd@gmail.com>
2023-04-25 21:59:14 +0000

    Font and CircleCI

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,29 +1,23 @@
 class WorkoutsController < ApplicationController
   before_action :set_workout, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, only: %i[ new edit create update destroy]
-
   # GET /workouts or /workouts.json
   def index
     @workouts = Workout.all
   end
-
   # GET /workouts/1 or /workouts/1.json
   def show
   end
-
   # GET /workouts/new
   def new
     @workout = Workout.new
   end
-
   # GET /workouts/1/edit
   def edit
   end
-
   # POST /workouts or /workouts.json
   def create
     @workout = Workout.new(workout_params)
-
     respond_to do |format|
       if @workout.save
         format.html { redirect_to workout_url(@workout), notice: "Workout was successfully created." }
@@ -34,7 +28,6 @@ class WorkoutsController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /workouts/1 or /workouts/1.json
   def update
     respond_to do |format|
@@ -47,7 +40,6 @@ class WorkoutsController < ApplicationController
       end
     end
   end
-
   # DELETE /workouts/1 or /workouts/1.json
   def destroy
     @workout.destroy
@@ -57,13 +49,11 @@ class WorkoutsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_workout
       @workout = Workout.find(params[:id])
     end
-
     # Only allow a list of trusted parameters through.
     def workout_params
       params.require(:workout).permit(:name, :date, :caloriesburned, :duration)
--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -1,24 +1,99 @@
 require 'rails_helper'
 
-# Change this ArticlesController to your project
 RSpec.describe WorkoutsController, type: :controller do
+  # Use Devise test helpers to log in a user before each test
+  before(:each) do
+    @user = FactoryBot.create(:user)
+    sign_in @user
+  end
 
-    login_user
+  let(:valid_attributes) {
+    { name: "test name", date: "1/1/2000", caloriesburned: 1, duration: "1 Hour" }
+  }
 
-    # This should return the minimal set of attributes required to create a valid
-    # Article. As you add validations to Article, be sure to adjust the attributes here as well.
-    let(:valid_attributes) {
-        { :name => "test name", :date => "1/1/2000", :caloriesburned => 1, :duration => "1 Hour" }
+  let(:invalid_attributes) {
+    { name: nil, date: nil, caloriesburned: nil, duration: nil }
+  }
+
+  describe "GET #index" do
+    it "returns a success response" do
+      Workout.create! valid_attributes
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      workout = Workout.create! valid_attributes
+      get :show, params: { id: workout.to_param }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #new" do
+    it "returns a success response" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns a success response" do
+      workout = Workout.create! valid_attributes
+      get :edit, params: { id: workout.to_param }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new workout" do
+        expect {
+          post :create, params: { workout: valid_attributes }
+        }.to change(Workout, :count).by(1)
+      end
+
+      it "redirects to the created workout" do
+        post :create, params: { workout: valid_attributes }
+        expect(response).to redirect_to(Workout.last)
+      end
+    end
+
+    context "with invalid params" do
+      it "returns a success response (i.e. to display the 'new' template)" do
+        post :create, params: { workout: invalid_attributes }
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    let(:new_attributes) {
+      { name: "new test name", date: "1/2/2000", caloriesburned: 2, duration: "2 Hours" }
     }
 
-    let(:valid_session) { {} }
+    context "with valid params" do
+      it "updates the requested workout" do
+        workout = Workout.create! valid_attributes
+        put :update, params: { id: workout.to_param, workout: new_attributes }
+        workout.reload
+        expect(workout.name).to eq("new test name")
+        expect(workout.caloriesburned).to eq(2)
+      end
 
-    describe "GET #index" do
-        it "returns a success response" do
-            Workout.create! valid_attributes
-            get :index, params: {}, session: valid_session
-            expect(response).to be_successful # be_successful expects a HTTP Status code of 200
-            # expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
+      it "redirects to the workout" do
+        workout = Workout.create! valid_attributes
+        put :update, params: { id: workout.to_param, workout: valid_attributes }
+        expect(response).to redirect_to(workout)
+      end
+      context "with invalid params" do
+        it "returns a success response (i.e. to display the 'edit' template)" do
+          workout = Workout.create! valid_attributes
+          put :update, params: { id: workout.to_param, workout: invalid_attributes }
+          expect(response).to be_successful
         end
     end
+end
+end
 end
